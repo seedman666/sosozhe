ca075ac446ecec7dec11c6bfd930812ce8479416
seed <seed@seeddeMacBook-Pro.local>
2014-11-05 08:34:24 +0800

    add searchview

--- sosozhe/IndexViewController.m
+++ sosozhe/IndexViewController.m
@@ -14,6 +14,7 @@
 #import "MBProgressHUD.h"
 #import "BrandView.h"
 #import "AppDelegate.h"
+#import <CommonCrypto/CommonDigest.h>
 
 @interface IndexViewController ()
 
---
+++ sosozhe/SearchResultViewController.h
@@ -0,0 +1,13 @@
+//
+//  SearchResultViewController.h
+//  sosozhe
+//
+//  Created by seed on 14-11-5.
+//  Copyright (c) 2014年 sosozhe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SearchResultViewController : UIViewController
+
+@end
